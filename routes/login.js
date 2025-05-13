62b4e0969aa908027d5528927e8a76b29b6d9ef0
Alex <168224523+skeletoboy@users.noreply.github.com>
2025-05-13 16:02:56 -0700

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+
 
 router.use(express.static(path.join(__dirname, "../public")));
 
@@ -23,7 +25,7 @@ db.exec(
         email TEXT UNIQUE NOT NULL,
         password TEXT NOT NULL
     )`,
-  (err) => {
+  (err) => {    
     if (err) {
       return console.error("Error creating table:", err.message);
     }
@@ -31,25 +33,31 @@ db.exec(
   }
 );
 
+
+
 //login form
 router.get("/login", (req, res) => {
   res.render('login', {error: null});
 });
-router.post("/login", (req, res) => {
+router.post("/login", async (req, res) => {
   try {
     const { username, password } = req.body;
     const q = "SELECT * FROM users WHERE username = ?";
-    db.get(q, [username], (err, row) => {
+    db.get(q, [username], async (err, row) => {
       if (err) {
         return res.status(500).send("Error");
       }
       if (!row) {
         return res.render('login', {error: 'Account does not exist'});
       }
-      if (row.password != password) {
-        return res.render('login', {error: 'Invalid Password'});
-      }
+      const comparePassword = await bcrypt.compare(password, row.password);
+      req.session.user = {
+        id: row.id,
+        username: row.username
+      };
+      if(comparePassword){
       res.redirect("/");
+    }
     });
   } catch (error) {
     console.error(error);
@@ -60,34 +68,37 @@ router.post("/login", (req, res) => {
 router.get("/register", (req, res) => {
   res.render("register");
 });
-router.post("/register", (req, res) => {
-  try {
+router.post("/register", async (req, res) => {
+//   try {
     const { username, email, password } = req.body;
+    const hashPassword = await bcrypt.hash(password, 10);
     const s = db.prepare(
       "INSERT INTO users (username, email, password) VALUES(?,?,?)"
     );
-    s.run(username, email, password, function (err) {
+    s.run(username, email, hashPassword, function (err) {
+        if (err){
       if (err.message.includes("UNIQUE constraint failed:users.username")) {
         res.send("Username already exists");
       }
       if (err.message.includes("UNIQUE constraint failed:users.email")) {
         res.send("Email already exists");
       }
-      res.redirect("/");
+    }
+      res.redirect("/login");
     });
-  } catch (error) {
-    console.log(error);
-  }
+//   } catch (error) {
+//     console.log(error);
+//   }
 });
 
-//check users
-// router.get("/users", (req, res) => {
-//   db.all("SELECT * FROM users", [], (err, rows) => {
-//     if (err) {
-//       return res.status(500).send(err.message);
-//     }
-//     res.json(rows);
-//   });
-// });
+// check users
+router.get("/users", (req, res) => {
+  db.all("SELECT * FROM users", [], (err, rows) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.json(rows);
+  });
+});
 
 module.exports = router;
