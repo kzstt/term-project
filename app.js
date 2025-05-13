81648dd348a1232c833d22768daf34d09abd77fe
Alex <168224523+skeletoboy@users.noreply.github.com>
2025-05-13 16:00:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,14 +2,27 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 const path = require('path');
+const session = require('express-session');
 
 // middleware
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(session({
+  secret: 'keyboard cat', // Change in production!
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true only with HTTPS
+}));
 
 // view
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
+// session
+app.use((req,res, next) => {
+  res.locals.user = req.session.user;
+  next();
+})
 
 // routes
 const loginRoutes = require('./routes/login');
@@ -22,6 +35,29 @@ app.get('/', (req, res) => {
 app.get('/login', (req, res) => {
   res.render('login')
 })
+
+app.get('/register', (req, res) => {
+  res.render('register');
+})
+
+const profileRoutes = require('./routes/profile');
+app.use('/profile', profileRoutes);
+app.get('/profile', (req, res) => {
+  if(res.session.user) {
+    res.render('profile')
+  } else {
+    res.render('/login')
+  }
+})
+
+// const productRoutes = require('./routes/products');
+// app.use('/', productRoutes);
+
+app.post('/logout', (req, res) => {
+  req.session.destroy(() => {
+    res.redirect('/');
+  });
+});
 
 
 app.listen(PORT, () => {
